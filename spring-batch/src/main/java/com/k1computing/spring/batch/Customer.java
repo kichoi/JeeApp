53ca267f0640f15db9d6021c18747713e6781220
kichoi <kchoi@k1computing.com>
2013-03-13 17:16:17 -0400

    Initial checkin

---
+++ spring-batch/src/main/java/com/k1computing/spring/batch/Customer.java
@@ -0,0 +1,25 @@
+package com.k1computing.spring.batch;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+	static final long serialVersionUID=1L;
+	
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
+}
